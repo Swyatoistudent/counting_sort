f45fb74d6390d0eec773ad2d6d802fe978d08a8e
Святослав <swyat.usatov@gmail.com>
2020-05-30 23:09:33 +0300

    add comment

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ unsigned long int array[] = {10,10,100};
 int n = sizeof(array)/ sizeof(int);
 int main() {
     int sorted[n]; // відсортований масив
-int min = array[0];
+int min = array[0]; // мінімальне  значення масиву для визначення розміру буферного масива
     max = array[0]; // максимальне значення масиву для визначення розміру буферного масива
 
     for(int i=0;i<n;i++){ // визначення макс елементу
@@ -17,19 +17,19 @@ int min = array[0];
             min=array[i];
         }
     }
-    int a[max-min];
+    int a[max-min]; //буферний масив
 
-    for(int i =0;i<max+1-min;i++){
+    for(int i =0;i<max+1-min;i++){// заповнення буферного масиву нулями
        a[i]=0;
     }
 
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++){// підрахування кількості елементів
         a[array[i]-min]+=1;
     }
 
     int iterator = 0;
 
-        for(int j =0;j<max+1-min;j++) {
+        for(int j =0;j<max+1-min;j++) {// заповнення выдсортованого масиву
             for(int i=0;i<a[j];i++){
 
                 sorted[iterator]=j+min;
@@ -38,9 +38,9 @@ int min = array[0];
         }
 
     for(int i=0;i<n;i++){
-        printf("%d ",sorted[i] );
+        printf("%d ",sorted[i] ); // вивід відсортованого масиву
 
     }
-    printf("\n %d",max-min);
+    printf("\nsize of buffer massive:  %d",sizeof(a)/sizeof(int));
     return 0;
 }
